15b150e3b0d54b54d8598e4b1b5a7e5b481d6106
Alison Lee <studycatcat@yahoo.com>
2022-03-21 18:22:54 -0700

    modified schema's id error and input one dummy data.

--- server/schema.sql
+++ server/schema.sql
@@ -3,11 +3,14 @@ CREATE DATABASE movielist;
 USE movielist;
 
 -- from sqldesigner
-CREATE TABLE `movies` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+CREATE TABLE `Movies` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NULL DEFAULT NULL,
   `watched` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- input default movie data below - optional:
+-- Is the below dummy data the solution to 'seed data into database'?
+-- should I add watched property here or it's just a control property in react front end only...
+INSERT INTO Movies (title, watched) VALUES ('The Big Short', 0);
